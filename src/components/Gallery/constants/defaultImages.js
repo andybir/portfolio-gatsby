77141fb6f1b534195540c586fe3b236eb8a89338
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2021-01-06 12:46:12 -0500

    added andyshop to portfolio

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -2,38 +2,42 @@ import thumb02 from '../../../assets/images/thumbs/museum-quiz.jpeg'
 import thumb01 from '../../../assets/images/thumbs/birdie-lou.png'
 import thumb03 from '../../../assets/images/thumbs/sethap.jpeg'
 import thumb04 from '../../../assets/images/thumbs/parent-helper.png'
-
+import thumb05 from '../../../assets/images/thumbs/andyshop-thumb.png'
 
 export const DEFAULT_IMAGES = [
-    {
-      id: '1',
-      thumbnail: thumb01,
-      caption: 'Birdie Lou',
-      description: 'A childrens\' vintage & handmade clothing website.',
-      url: 'https://birdie-lou.com',
-    },
-    {
-      id: '2',
-      thumbnail: thumb02,
-      caption: 'Museum Quiz',
-      description: 'Explore antiquities & artifacts from ancient civilizations.',
-      url: 'http://museum-quiz.surge.sh',
-
-    },
-    {
-      id: '3',
-      thumbnail: thumb03,
-      caption: 'SetHap',
-      description: 'Seach for a band, learn about a world event.',
-      url: 'http://sethap.surge.sh',
-
-    },
-    {
-      id: '4',
-      thumbnail: thumb04,
-      caption: 'Parent Helper',
-      description: 'A forum for parents to get advice on various topics.',
-      url: 'https://parent-helper-63190.herokuapp.com/account',
-
-    }
+  {
+    id: '1',
+    thumbnail: thumb05,
+    caption: 'Andyshop',
+    description: 'An ecommerce website',
+    url: 'https://andyshopapp.herokuapp.com/',
+  },
+  {
+    id: '2',
+    thumbnail: thumb01,
+    caption: 'Birdie Lou',
+    description: "A childrens' vintage & handmade clothing website.",
+    url: 'https://birdie-lou.com',
+  },
+  {
+    id: '3',
+    thumbnail: thumb02,
+    caption: 'Museum Quiz',
+    description: 'Explore antiquities & artifacts from ancient civilizations.',
+    url: 'http://museum-quiz.surge.sh',
+  },
+  {
+    id: '4',
+    thumbnail: thumb03,
+    caption: 'SetHap',
+    description: 'Seach for a band, learn about a world event.',
+    url: 'http://sethap.surge.sh',
+  },
+  {
+    id: '5',
+    thumbnail: thumb04,
+    caption: 'Parent Helper',
+    description: 'A forum for parents to get advice on various topics.',
+    url: 'https://parent-helper-63190.herokuapp.com/account',
+  },
 ]
