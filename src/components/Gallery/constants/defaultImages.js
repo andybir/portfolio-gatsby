609529e84078a017e0575d8cc762d6c8d34f2318
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2021-02-06 19:25:32 -0500

    updated andyshop url

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -10,7 +10,7 @@ export const DEFAULT_IMAGES = [
     thumbnail: thumb05,
     caption: 'Andyshop',
     description: 'An ecommerce website',
-    url: 'https://andyshopapp.herokuapp.com/',
+    url: 'https://shop.andybirosak.com/',
   },
   {
     id: '2',
