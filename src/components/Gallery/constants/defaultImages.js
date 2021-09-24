72236ef401f90b953d80463f7d7c4cdfd913b3b3
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2021-09-24 15:46:41 -0400

    added hollow to projects

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -3,38 +3,46 @@ import thumb01 from '../../../assets/images/thumbs/birdie-lou.png'
 import thumb03 from '../../../assets/images/thumbs/sethap.jpeg'
 import thumb04 from '../../../assets/images/thumbs/parent-helper.png'
 import thumb05 from '../../../assets/images/thumbs/andyshop-thumb.png'
+import thumb06 from '../../../assets/images/thumbs/the-hollow-homepage.png'
 
 export const DEFAULT_IMAGES = [
   {
     id: '1',
+    thumbnail: thumb06,
+    caption: 'The Hollow',
+    description: 'A play cafe based in Brooklyn, NY',
+    url: 'https://thehollowbk.org',
+  },
+  {
+    id: '2',
     thumbnail: thumb01,
     caption: 'Birdie Lou',
     description: "A childrens' vintage & handmade clothing website.",
     url: 'https://birdie-lou.com',
   },
   {
-    id: '2',
+    id: '3',
     thumbnail: thumb05,
     caption: 'Andyshop',
     description: 'An ecommerce website',
     url: 'http://shop.andybirosak.com/',
   },
   {
-    id: '3',
+    id: '4',
     thumbnail: thumb02,
     caption: 'Museum Quiz',
     description: 'Explore antiquities & artifacts from ancient civilizations.',
     url: 'http://museum-quiz.surge.sh',
   },
   {
-    id: '4',
+    id: '5',
     thumbnail: thumb03,
     caption: 'SetHap',
     description: 'Seach for a band, learn about a world event.',
     url: 'http://sethap.surge.sh',
   },
   {
-    id: '5',
+    id: '6',
     thumbnail: thumb04,
     caption: 'Parent Helper',
     description: 'A forum for parents to get advice on various topics.',
